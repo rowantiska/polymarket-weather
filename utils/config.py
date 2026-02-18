c5607720cdcec90257c6525f60b37edecf8f6df9
Rowan Tiska <rowan@Rowans-MacBook-Air.local>
2026-02-18 09:16:18 -0500

    upload

--- utils/config.py
+++ utils/config.py
@@ -1,5 +1,2 @@
 #general
-SLACK_WEBHOOK = "https://hooks.slack.com/services/"
-
-#copy-cat
-MAX_TRADE_TOTAL = 30 #USD / $
+SLACK_WEBHOOK = ""
--- utils/webhook.py
+++ utils/webhook.py
@@ -2,7 +2,7 @@ import requests
 
 def webhook(message):
     payload = { "text": message }
-    response = requests.post("https://hooks.slack.com/services/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", json=payload)
+    response = requests.post("", json=payload)
     if response.status_code == 200:
         return
     else:
